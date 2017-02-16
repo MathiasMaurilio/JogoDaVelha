4eb2e9422835b64109c286ebfe5f87d2de9ded3e
Mathias S Maurilio <maurilio.1996.mathias@gmail.com>
2017-02-16 16:25:42 -0200

    Adding the project to git
    copying all files of the initial project for git

---
+++ Form1.Designer.cs
@@ -0,0 +1,272 @@
+﻿namespace JogoDaVelha
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnReiniciar = new System.Windows.Forms.Button();
+            this.btnNovo = new System.Windows.Forms.Button();
+            this.imageX = new System.Windows.Forms.PictureBox();
+            this.imageO = new System.Windows.Forms.PictureBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btn9 = new System.Windows.Forms.Button();
+            this.btn8 = new System.Windows.Forms.Button();
+            this.btn7 = new System.Windows.Forms.Button();
+            this.btn6 = new System.Windows.Forms.Button();
+            this.btn5 = new System.Windows.Forms.Button();
+            this.btn4 = new System.Windows.Forms.Button();
+            this.btn3 = new System.Windows.Forms.Button();
+            this.btn2 = new System.Windows.Forms.Button();
+            this.btn1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.imageX)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imageO)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(483, 114);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(68, 13);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "O Ganhou: 0";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(483, 145);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(67, 13);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "X Ganhou: 0";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(483, 54);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(50, 13);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "Vez do X";
+            // 
+            // btnReiniciar
+            // 
+            this.btnReiniciar.Location = new System.Drawing.Point(464, 289);
+            this.btnReiniciar.Name = "btnReiniciar";
+            this.btnReiniciar.Size = new System.Drawing.Size(161, 23);
+            this.btnReiniciar.TabIndex = 12;
+            this.btnReiniciar.Text = "Reiniciar";
+            this.btnReiniciar.UseVisualStyleBackColor = true;
+            this.btnReiniciar.Click += new System.EventHandler(this.btnReiniciar_Click);
+            // 
+            // btnNovo
+            // 
+            this.btnNovo.Location = new System.Drawing.Point(464, 253);
+            this.btnNovo.Name = "btnNovo";
+            this.btnNovo.Size = new System.Drawing.Size(161, 23);
+            this.btnNovo.TabIndex = 13;
+            this.btnNovo.Text = "Novo";
+            this.btnNovo.UseVisualStyleBackColor = true;
+            this.btnNovo.Click += new System.EventHandler(this.btnNovo_Click);
+            // 
+            // imageX
+            // 
+            this.imageX.Image = global::JogoDaVelha.Properties.Resources.icon2;
+            this.imageX.InitialImage = global::JogoDaVelha.Properties.Resources.icon2;
+            this.imageX.Location = new System.Drawing.Point(48, 54);
+            this.imageX.Name = "imageX";
+            this.imageX.Size = new System.Drawing.Size(100, 91);
+            this.imageX.TabIndex = 14;
+            this.imageX.TabStop = false;
+            // 
+            // imageO
+            // 
+            this.imageO.Image = global::JogoDaVelha.Properties.Resources.orkut_letter_logo2;
+            this.imageO.InitialImage = null;
+            this.imageO.Location = new System.Drawing.Point(154, 54);
+            this.imageO.Name = "imageO";
+            this.imageO.Size = new System.Drawing.Size(88, 91);
+            this.imageO.TabIndex = 15;
+            this.imageO.TabStop = false;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.btn9);
+            this.panel1.Controls.Add(this.btn8);
+            this.panel1.Controls.Add(this.btn7);
+            this.panel1.Controls.Add(this.btn6);
+            this.panel1.Controls.Add(this.btn5);
+            this.panel1.Controls.Add(this.btn4);
+            this.panel1.Controls.Add(this.btn3);
+            this.panel1.Controls.Add(this.btn2);
+            this.panel1.Controls.Add(this.btn1);
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(394, 319);
+            this.panel1.TabIndex = 18;
+            // 
+            // btn9
+            // 
+            this.btn9.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn9.Location = new System.Drawing.Point(263, 211);
+            this.btn9.Name = "btn9";
+            this.btn9.Size = new System.Drawing.Size(122, 96);
+            this.btn9.TabIndex = 8;
+            this.btn9.UseVisualStyleBackColor = true;
+            this.btn9.Click += new System.EventHandler(this.btn9_Click);
+            // 
+            // btn8
+            // 
+            this.btn8.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn8.Location = new System.Drawing.Point(135, 211);
+            this.btn8.Name = "btn8";
+            this.btn8.Size = new System.Drawing.Size(122, 96);
+            this.btn8.TabIndex = 7;
+            this.btn8.UseVisualStyleBackColor = true;
+            this.btn8.Click += new System.EventHandler(this.btn8_Click);
+            // 
+            // btn7
+            // 
+            this.btn7.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn7.Location = new System.Drawing.Point(7, 211);
+            this.btn7.Name = "btn7";
+            this.btn7.Size = new System.Drawing.Size(122, 96);
+            this.btn7.TabIndex = 6;
+            this.btn7.UseVisualStyleBackColor = true;
+            this.btn7.Click += new System.EventHandler(this.btn7_Click);
+            // 
+            // btn6
+            // 
+            this.btn6.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn6.Location = new System.Drawing.Point(263, 109);
+            this.btn6.Name = "btn6";
+            this.btn6.Size = new System.Drawing.Size(122, 96);
+            this.btn6.TabIndex = 5;
+            this.btn6.UseVisualStyleBackColor = true;
+            this.btn6.Click += new System.EventHandler(this.btn6_Click);
+            // 
+            // btn5
+            // 
+            this.btn5.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn5.Location = new System.Drawing.Point(135, 109);
+            this.btn5.Name = "btn5";
+            this.btn5.Size = new System.Drawing.Size(122, 96);
+            this.btn5.TabIndex = 4;
+            this.btn5.UseVisualStyleBackColor = true;
+            this.btn5.Click += new System.EventHandler(this.btn5_Click);
+            // 
+            // btn4
+            // 
+            this.btn4.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn4.Location = new System.Drawing.Point(7, 109);
+            this.btn4.Name = "btn4";
+            this.btn4.Size = new System.Drawing.Size(122, 96);
+            this.btn4.TabIndex = 3;
+            this.btn4.UseVisualStyleBackColor = true;
+            this.btn4.Click += new System.EventHandler(this.btn4_Click);
+            // 
+            // btn3
+            // 
+            this.btn3.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn3.Location = new System.Drawing.Point(263, 7);
+            this.btn3.Name = "btn3";
+            this.btn3.Size = new System.Drawing.Size(122, 96);
+            this.btn3.TabIndex = 2;
+            this.btn3.UseVisualStyleBackColor = true;
+            this.btn3.Click += new System.EventHandler(this.btn3_Click);
+            // 
+            // btn2
+            // 
+            this.btn2.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn2.Location = new System.Drawing.Point(135, 7);
+            this.btn2.Name = "btn2";
+            this.btn2.Size = new System.Drawing.Size(122, 96);
+            this.btn2.TabIndex = 1;
+            this.btn2.UseVisualStyleBackColor = true;
+            this.btn2.Click += new System.EventHandler(this.btn2_Click);
+            // 
+            // btn1
+            // 
+            this.btn1.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn1.Location = new System.Drawing.Point(7, 7);
+            this.btn1.Name = "btn1";
+            this.btn1.Size = new System.Drawing.Size(122, 96);
+            this.btn1.TabIndex = 0;
+            this.btn1.UseVisualStyleBackColor = true;
+            this.btn1.Click += new System.EventHandler(this.btn1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(637, 341);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.imageO);
+            this.Controls.Add(this.imageX);
+            this.Controls.Add(this.btnNovo);
+            this.Controls.Add(this.btnReiniciar);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.imageX)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imageO)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnReiniciar;
+        private System.Windows.Forms.Button btnNovo;
+        private System.Windows.Forms.PictureBox imageX;
+        private System.Windows.Forms.PictureBox imageO;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btn9;
+        private System.Windows.Forms.Button btn8;
+        private System.Windows.Forms.Button btn7;
+        private System.Windows.Forms.Button btn6;
+        private System.Windows.Forms.Button btn5;
+        private System.Windows.Forms.Button btn4;
+        private System.Windows.Forms.Button btn3;
+        private System.Windows.Forms.Button btn2;
+        private System.Windows.Forms.Button btn1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,276 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace JogoDaVelha
+{
+    public partial class Form1 : Form
+    {
+        string jogador = "X";
+        int vezesXGanhou = 0;
+        int vezesOGanhou = 0;
+        int contador = 0;
+        Image imgX, imgO;
+        //Image imgX = Image.FromFile(@"E:\Documents\Visual Studio 2015\Projects\JogoDaVelha\JogoDaVelha\Resources\icon2.png");
+        //Image imgO = Image.FromFile(@"E:\Documents\Visual Studio 2015\Projects\JogoDaVelha\JogoDaVelha\Resources\orkut-letter-logo2.png");
+
+        public Form1()
+        {
+            InitializeComponent();
+            imgX = imageX.Image;
+            imgO = imageO.Image;
+        }
+
+        private void btnNovo_Click(object sender, EventArgs e)
+        {
+            limparCampos();
+            panel1.Enabled = true;
+            label3.Text = "Vez do X";
+            jogador = "X";
+            contador = 0;
+        }
+
+        private void btnReiniciar_Click(object sender, EventArgs e)
+        {
+            limparCampos();
+            label1.Text = "O Ganhou: 0";
+            label2.Text = "X Ganhou: 0";
+            vezesOGanhou = 0;
+            vezesXGanhou = 0;
+            panel1.Enabled = true;
+            label3.Text = "Vez do X";
+            jogador = "X";
+            contador = 0;
+        }
+
+        public void limparCampos()
+        {
+            btn1.Image = null;
+            btn2.Image = null;
+            btn3.Image = null;
+            btn4.Image = null;
+            btn5.Image = null;
+            btn6.Image = null;
+            btn7.Image = null;
+            btn8.Image = null;
+            btn9.Image = null;
+        }
+
+        public void ganhador()
+        {
+            if (jogador == "X")
+            {
+                vezesXGanhou++;
+                label2.Text = "X Ganhou: " + Convert.ToString(vezesXGanhou);
+            }
+            else
+            {
+                vezesOGanhou++;
+                label1.Text = "O Ganhou: " + Convert.ToString(vezesOGanhou);
+            }
+            MessageBox.Show(jogador + " Venceu");
+            string escolhaNovo = MessageBox.Show("Jogar Novamente?", "Novo Jogo", MessageBoxButtons.YesNo).ToString();
+            if (escolhaNovo == "Yes")
+            {
+                limparCampos();
+                contador = 0;
+            }
+            else if(escolhaNovo == "No")
+            {
+                panel1.Enabled = false;
+            }
+        }
+
+        public bool testarCampos(Image primeiro, Image segundo, Image terceiro)
+        {
+            if (primeiro == imgX && segundo == imgX && terceiro == imgX)
+                return true;
+            else if (primeiro == imgO && segundo == imgO && terceiro == imgO)
+                return true;
+            else
+                return false;
+        }
+
+        public void mudarJogador()
+        {
+            if(contador >= 9)
+            {
+                string novoJogo = MessageBox.Show("Empate\nNovo Jogo?", "Empate", MessageBoxButtons.YesNo).ToString();
+                if(novoJogo == "Yes")
+                {
+                    limparCampos();
+                    contador = 0;
+                }
+                else if(novoJogo == "No")
+                {
+                    panel1.Enabled = false;
+                }
+            }
+            else
+            {
+                if (string.Equals(btn1.Image, btn2.Image) && string.Equals(btn1.Image, btn3.Image))
+                    if (testarCampos(btn1.Image, btn2.Image, btn3.Image))
+                        ganhador();
+                if (string.Equals(btn4.Image, btn5.Image) && string.Equals(btn4.Image, btn6.Image))
+                    if (testarCampos(btn4.Image, btn5.Image, btn6.Image))
+                        ganhador();
+                if (string.Equals(btn7.Image, btn8.Image) && string.Equals(btn7.Image, btn9.Image))
+                    if (testarCampos(btn7.Image, btn8.Image, btn9.Image))
+                        ganhador();
+
+                if (string.Equals(btn1.Image, btn4.Image) && string.Equals(btn1.Image, btn7.Image))
+                    if (testarCampos(btn1.Image, btn4.Image, btn7.Image))
+                        ganhador();
+                if (string.Equals(btn2.Image, btn5.Image) && string.Equals(btn2.Image, btn8.Image))
+                    if (testarCampos(btn2.Image, btn5.Image, btn8.Image))
+                        ganhador();
+                if (string.Equals(btn3.Image, btn6.Image) && string.Equals(btn3.Image, btn9.Image))
+                    if (testarCampos(btn3.Image, btn6.Image, btn9.Image))
+                        ganhador();
+
+                if (string.Equals(btn1.Image, btn5.Image) && string.Equals(btn1.Image, btn9.Image))
+                    if (testarCampos(btn1.Image, btn5.Image, btn9.Image))
+                        ganhador();
+                if (string.Equals(btn3.Image, btn5.Image) && string.Equals(btn3.Image, btn7.Image))
+                    if (testarCampos(btn3.Image, btn5.Image, btn7.Image))
+                        ganhador();
+                if (jogador == "X")
+                {
+                    jogador = "O";
+                }
+                else
+                {
+                    jogador = "X";
+                }
+                label3.Text = "Vez do " + jogador;
+                if (panel1.Enabled == false)
+                {
+                    label3.Text = "Vez do ";
+                }
+            }
+        }
+
+        private void btn1_Click(object sender, EventArgs e)
+        {
+            if (btn1.Image == null)
+            {
+                contador++;
+                if (jogador == "X")
+                    btn1.Image = imgX;
+                else
+                    btn1.Image = imgO;
+                mudarJogador();
+            }
+        }
+
+        private void btn2_Click(object sender, EventArgs e)
+        {
+            if (btn2.Image == null)
+            {
+                contador++;
+                if (jogador == "X")
+                    btn2.Image = imgX;
+                else
+                    btn2.Image = imgO;
+                mudarJogador();
+            }
+        }
+
+        private void btn3_Click(object sender, EventArgs e)
+        {
+            if (btn3.Image == null)
+            {
+                contador++;
+                if (jogador == "X")
+                    btn3.Image = imgX;
+                else
+                    btn3.Image = imgO;
+                mudarJogador();
+            }
+        }
+
+        private void btn4_Click(object sender, EventArgs e)
+        {
+            if (btn4.Image == null)
+            {
+                contador++;
+                if (jogador == "X")
+                    btn4.Image = imgX;
+                else
+                    btn4.Image = imgO;
+                mudarJogador();
+            }
+        }
+
+        private void btn5_Click(object sender, EventArgs e)
+        {
+            if (btn5.Image == null)
+            {
+                contador++;
+                if (jogador == "X")
+                    btn5.Image = imgX;
+                else
+                    btn5.Image = imgO;
+                mudarJogador();
+            }
+        }
+
+        private void btn6_Click(object sender, EventArgs e)
+        {
+            if (btn6.Image == null)
+            {
+                contador++;
+                if (jogador == "X")
+                    btn6.Image = imgX;
+                else
+                    btn6.Image = imgO;
+                mudarJogador();
+            }
+        }
+
+        private void btn7_Click(object sender, EventArgs e)
+        {
+            if (btn7.Image == null)
+            {
+                contador++;
+                if (jogador == "X")
+                    btn7.Image = imgX;
+                else
+                    btn7.Image = imgO;
+                mudarJogador();
+            }
+        }
+
+        private void btn8_Click(object sender, EventArgs e)
+        {
+            if (btn8.Image == null)
+            {
+                contador++;
+                if (jogador == "X")
+                    btn8.Image = imgX;
+                else
+                    btn8.Image = imgO;
+                mudarJogador();
+            }
+        }
+
+        private void btn9_Click(object sender, EventArgs e)
+        {
+            if (btn9.Image == null)
+            {
+                contador++;
+                if (jogador == "X")
+                    btn9.Image = imgX;
+                else
+                    btn9.Image = imgO;
+                mudarJogador();
+            }
+        }
+    }
+}
